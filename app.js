4b0f4ab728e0fafd5168b842afd2ab364c92dcad
peiyi-c <pennieyoyoyo@gmail.com>
2023-06-11 14:26:14 +0200

    structure data

--- app.js
+++ app.js
@@ -1,30 +1,7 @@
+/* DATA */
+import { companies, clients, works } from './assets/js/data.js'
+
 /*  COMPANIES CONTENT  */
-const companies = [
-  {
-    id: 1,
-    name: "Microsoft"
-  },
-  {
-    id: 2,
-    name: "Goggle"
-  },
-  {
-    id: 3,
-    name: "Walmart"
-  },
-  {
-    id: 4,
-    name: "airbnb"
-  },
-  {
-    id: 5,
-    name: "amazon"
-  },
-  {
-    id: 6,
-    name: "CNN"
-  },
-];
 
 const companyContent = document.querySelector('.company-content')
 
@@ -47,37 +24,6 @@ function renderCompanyContent(items) {
 renderCompanyContent(companies)
 
 /*  CLIENT CONTENT  */
-
-const clients = [
-  {
-    id: 1,
-    name: "Johnathan Zawadi",
-    job: "CEO & Founder",
-    rating: 4,
-    quote: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt recusandae amet dignissimos, ullam tenetur optio, nulla sapiente odit, rem cum expedita!"
-  },
-  {
-    id: 2,
-    name: "Joy Marette",
-    job: "Global Finance Manager",
-    rating: 5,
-    quote: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt recusandae amet dignissimos, ullam tenetur optio, nulla sapiente odit, rem cum expedita!"
-  },
-  {
-    id: 3,
-    name: "Carebell Aberdom",
-    job: "Entrepreneur",
-    rating: 4,
-    quote: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt recusandae amet dignissimos, ullam tenetur optio, nulla sapiente odit, rem cum expedita!"
-  },
-  {
-    id: 4,
-    name: "Uhuru Kimadi",
-    job: "General Manager",
-    rating: 5,
-    quote: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt recusandae amet dignissimos, ullam tenetur optio, nulla sapiente odit, rem cum expedita!"
-  },
-];
 
 /* content */
 
@@ -138,55 +84,6 @@ function generateRatings(num) {
 renderRatings(clients)
 
 /*  PROJECT CONTENT  */
-
-const works = [
-  {
-    id: 1,
-    name: "project ninja",
-    category: "Websites"
-  },
-  {
-    id: 2,
-    name: "shape&fit",
-    category: "Websites"
-  },
-  {
-    id: 3,
-    name: "fish and chips",
-    category: "Websites"
-  },
-  {
-    id: 4,
-    name: "fairytale hair",
-    category: "Design"
-  },
-  {
-    id: 5,
-    name: "design gururu",
-    category: "Design"
-  },
-  {
-    id: 6,
-    name: "coffee host",
-    category: "Mockup"
-  },
-  {
-    id: 7,
-    name: "slurpee genie",
-    category: "Mockup"
-  },
-  {
-    id: 8,
-    name: "cornflakes snowflakes",
-    category: "Mockup"
-  },
-  {
-    id: 9,
-    name: "Penny's cheese factory",
-    category: "Mockup"
-  }
-];
-
 const projectContent = document.querySelector('.project-content')
 const filterBtns = document.querySelector('.filter-btns')
 
---
+++ assets/js/data.js
@@ -0,0 +1,107 @@
+const companies = [
+  {
+    id: 1,
+    name: "Microsoft"
+  },
+  {
+    id: 2,
+    name: "Goggle"
+  },
+  {
+    id: 3,
+    name: "Walmart"
+  },
+  {
+    id: 4,
+    name: "airbnb"
+  },
+  {
+    id: 5,
+    name: "amazon"
+  },
+  {
+    id: 6,
+    name: "CNN"
+  },
+];
+
+const clients = [
+  {
+    id: 1,
+    name: "Johnathan Zawadi",
+    job: "CEO & Founder",
+    rating: 4,
+    quote: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt recusandae amet dignissimos, ullam tenetur optio, nulla sapiente odit, rem cum expedita!"
+  },
+  {
+    id: 2,
+    name: "Joy Marette",
+    job: "Global Finance Manager",
+    rating: 5,
+    quote: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt recusandae amet dignissimos, ullam tenetur optio, nulla sapiente odit, rem cum expedita!"
+  },
+  {
+    id: 3,
+    name: "Carebell Aberdom",
+    job: "Entrepreneur",
+    rating: 4,
+    quote: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt recusandae amet dignissimos, ullam tenetur optio, nulla sapiente odit, rem cum expedita!"
+  },
+  {
+    id: 4,
+    name: "Uhuru Kimadi",
+    job: "General Manager",
+    rating: 5,
+    quote: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt recusandae amet dignissimos, ullam tenetur optio, nulla sapiente odit, rem cum expedita!"
+  },
+];
+
+const works = [
+  {
+    id: 1,
+    name: "project ninja",
+    category: "Websites"
+  },
+  {
+    id: 2,
+    name: "shape&fit",
+    category: "Websites"
+  },
+  {
+    id: 3,
+    name: "fish and chips",
+    category: "Websites"
+  },
+  {
+    id: 4,
+    name: "fairytale hair",
+    category: "Design"
+  },
+  {
+    id: 5,
+    name: "design gururu",
+    category: "Design"
+  },
+  {
+    id: 6,
+    name: "coffee host",
+    category: "Mockup"
+  },
+  {
+    id: 7,
+    name: "slurpee genie",
+    category: "Mockup"
+  },
+  {
+    id: 8,
+    name: "cornflakes snowflakes",
+    category: "Mockup"
+  },
+  {
+    id: 9,
+    name: "Penny's cheese factory",
+    category: "Mockup"
+  }
+];
+
+export { companies, clients, works };
